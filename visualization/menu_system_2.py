51a5a568be5a6db9463a6117e8c1281c7cf2fe4d
jimenezsilvara <76070145+jimenezsilvara@users.noreply.github.com>
2021-09-14 21:17:41 -0400

    Update menu_system_2.py
    added toolbar functionalities

--- visualization/menu_system_2.py
+++ visualization/menu_system_2.py
@@ -51,7 +51,8 @@
 
 import os, sys, tty ,termios
 from termcolor import colored
-
+import copy
+import string
 
 class KeyboardReader():
     def __init__(self):
@@ -77,17 +78,123 @@ class KeyboardReader():
         finally:
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
+class MenuPrint():
+
+    def __init__(self, output=None, settings=None):
+        self.output = '' if output is None else output
+        self.settings = settings
+
+    def print_in_console(self):
+        print('{}'.format(self.settings.format_str(self.output)))
+
+
+class MenuToolBarOptions():
+    def __init__(self, option_rack = None):
+        self.option_rack = option_rack if option_rack else []
+        self.title = self.option_rack[0] if self.option_rack else ''
+
+    def show_options(self, x = None):
+
+        if x is None:
+            pass
+        else:
+            l = self.option_rack
+            current_option = x.title
+            #var = current_option
+            if not current_option in l:
+                pass
+            else:
+                current_option_index = l.index(current_option)
+                if current_option_index == len(l)-1:
+                    next_option_index = 0
+                else:
+                    next_option_index = current_option_index + 1
+                return l[next_option_index]
+    def return_options(self):
+        return self.option_rack
+
+
+class MenuToolBar():
+    
+
+    def __init__(self, title = None, options = None, previous_execution = None, highlighted_option = None, text_settings = None, selection_settings = None, slots=None):
+        
+        self.title = title
+        self.options = options
+        self.slots = string.ascii_lowercase[0: len(self.options)] if slots is None else slots
+        self.default_options = copy.deepcopy(options)
+        self.previous_execution = MenuAction()
+        self.highlighted_option = MenuAction()
+        self.text_settings = MenuSettings(color='white', attributes=[]) if text_settings is None else text_settings
+        
+        self.selection_settings = MenuSettings(color='white', attributes=['reverse', 'bold']) if selection_settings is None else selection_settings
+
+    def generate_toolbar(self):
+
+        bar = [self.selection_settings.format_str(o.title) if o == self.highlighted_option else self.text_settings.format_str(o.title) for o in self.options]
+        assert len(self.slots) == len(bar)
+        bar = ['{}:{}'.format(self.text_settings.format_str(x), y) for x, y in zip(self.slots, bar)]
+        print('  '.join(bar))
+
+    def reset_toolbar(self):
+
+        if self.previous_execution:
+    
+            try:
+                highlighted_option_index = self.options.index(self.highlighted_option)
+                self.options = [MenuAction(do.title, o.action) for o, do in zip(self.options, self.default_options)]
+                self.highlighted_option = self.options[highlighted_option_index]
+            except ValueError as e:
+                self.options = [MenuAction(do.title, o.action) for o, do in zip(self.options, self.default_options)]
+
+
+    def update_toolbar(self, x=None):
+
+        if not x is None:
+            try:
+                current_option_index = self.options.index(self.highlighted_option)
+
+            except ValueError as e:
+
+                current_option_index = 0
+            
+            if current_option_index == 0:
+                if x < 0:
+                    self.highlighted_option = self.options[len(self.options) - 1]
+                if x > 0:
+                    self.highlighted_option = self.options[current_option_index + x]
+
+            if current_option_index == len(self.options) - 1:
+                if x < 0:
+                    
+                    self.highlighted_option = self.options[current_option_index + x]
+                if x > 0: 
+                    self.highlighted_option = self.options[0]
+
+            if current_option_index + x in range(0, len(self.options)):
+                #current_option_index = current_option_index + x
+                self.highlighted_option = self.options[current_option_index + x]
+
+
+            #self.highlighted_option = MenuAction()
+
+        
+
+
 
 class MenuAction():
     def __init__(self, title=None, action=None):
         self.title = 'default_option_title' if not title else title
         self.action = action
+        self.highlighted_option = None
 
     def __repr__(self):
         """
         :type: MenuAction
         """
         return '{}'.format(self.title.title())
+
+
 
     @property
     def title(self):
@@ -127,14 +234,21 @@ class MenuAction():
         """
         del self._function
 
-
-
-    def execute_action(self):
+    def execute_action(self, x=None):
         assert hasattr(self.action, '__call__') or self.action is None, 'menu action is not a function'
         if self.action is None:
             pass
         else:
-            self.action()
+            try:
+                return self.action(x)
+            except TypeError as e:
+                return self.action()
+
+
+
+    def highlight_action(self, settings):
+        self.title = settings.format_str(self.title)
+        self.highlighted_option = True
 
 class MenuSettings():
     def __init__(self, color=None, attributes=None):
@@ -158,7 +272,7 @@ class Menu():
     :type child_menus: dict
 
     """
-    def __init__(self, title=None, options=None, selection_settings=None, text_settings=None, inherit_settings=False):
+    def __init__(self, title=None, options=None, selection_settings=None, text_settings=None, inherit_settings=False, tool_bar=None):
         self.title = title
         self.options = {x.title:x for x in options}
         self.parent_menu = None
@@ -169,6 +283,7 @@ class Menu():
         self.text_settings = MenuSettings(color='white', attributes=[]) if text_settings is None else text_settings
         
         self.highlighted_option = self.options[list(self.options.keys())[0]]
+        self.tool_bar = tool_bar
 
         self.create_links()
 
@@ -406,13 +521,17 @@ class Menu():
             return m if m.parent_menu is None else root_search(m.parent_menu)
         return root_search(self)
         
-    def run_menu(self):
+    def run_menu(self, input_ = True):
         '''
             Runs the current menu
         '''
-        self.clear_screen()
 
+        self.clear_screen()
         print(self.text_settings.format_str('{}'.format(self.title.title())))
+        if self.tool_bar:
+            #print(self.tool_bar.options)
+            #self.tool_bar.options filters
+            self.tool_bar.generate_toolbar()
         print(self.text_settings.format_str('Please select an option:\n'))
 
         for option in self.options.keys():
@@ -435,9 +554,13 @@ class Menu():
             print(self.text_settings.format_str('Press enter to execute the option.'))
         print('\n')
         if isinstance(self.previous_execution, MenuAction):
-            self.previous_execution.execute_action()
+            if self.tool_bar: 
+                MenuPrint(self.previous_execution.execute_action(self.tool_bar.options), self.text_settings).print_in_console()
+            else:
+                MenuPrint(self.previous_execution.execute_action(), self.text_settings).print_in_console()
 
-        self.read_input()
+        if input_:
+            self.read_input()
 
     def read_input(self):
         '''
@@ -466,9 +589,26 @@ class Menu():
 
     def update_movement(self, key):
 
-        x = {'up':-1, 'down':1}[key]
-            
+        try:
+            x = {'up':-1, 'down':1}[key]
+        except KeyError as e:
+            x = 0
+        try:
+            y = {'left':-1, 'right':1}[key]
+        except KeyError as e:
+            y = 0
+
         options_keys = list(self.options.keys())
+
+        if not isinstance(self.highlighted_option, Menu):
+            if self.tool_bar:
+                if y in [1, -1] and not self.highlighted_option.action:
+                    
+                    self.tool_bar.update_toolbar(y) # moves left and
+                    self.run_menu(input_=False)
+                elif y == 0:
+                    self.tool_bar.highlighted_option = MenuAction()
+
 
         try:
             current_option_index = options_keys.index(self.highlighted_option.title)
@@ -477,6 +617,12 @@ class Menu():
 
         if current_option_index  == len(options_keys) - 1: # last item in the options list
             if x > 0:
+                if self.tool_bar:
+                    
+                    self.highlighted_option = MenuAction()
+                    self.tool_bar.update_toolbar(0)
+                    self.refresh()
+                    
                 current_option_index = 0
                 self.highlighted_option = self.options[options_keys[current_option_index]]
                 if not self.has_parent():
@@ -484,12 +630,22 @@ class Menu():
                 self.refresh()
 
             elif x < 0:
+                if self.tool_bar:
+                    
+                    self.highlighted_option = MenuAction()
+                    self.tool_bar.update_toolbar(0)
+                    self.refresh()
                 current_option_index += x
                 self.highlighted_option = self.options[options_keys[current_option_index]]
                 self.refresh()
 
         if current_option_index == 0: # first item in the options list
             if x < 0:
+                if self.tool_bar:
+                    self.highlighted_option = MenuAction()
+                    self.tool_bar.update_toolbar(0)
+                    self.refresh()
+                    
                 current_option_index = len(options_keys) - 1
                 current_menu_action = self.options[options_keys[current_option_index]]
                 self.highlighted_option = self.options[options_keys[current_option_index]]
@@ -518,10 +674,14 @@ class Menu():
             Validates the key pressed by the user
 
         '''
-        if key in ['up', 'down']:
+        if key in ['up', 'down', 'left', 'right']:
             self.update_movement(key)
+            
         elif key == 'space': 
             self.previous_execution = MenuAction()
+            if self.tool_bar:
+                self.tool_bar.reset_toolbar()
+                
             self.refresh()
         elif key == 'esc' and not self.has_parent(): 
             self.exit()
@@ -529,6 +689,40 @@ class Menu():
             self.previous_execution = MenuAction() # clears the output every time one goes back and forth menus
             self.back()
         elif key == 'enter':
+            if self.tool_bar:
+                if self.tool_bar.highlighted_option.action: 
+                    current_option = self.tool_bar.highlighted_option
+
+                    current_option_stack = current_option.execute_action()
+
+                    while(True):
+                        key = KeyboardReader().read_key()
+                        if key == 'enter':
+                            self.refresh()
+                        if key in ['left', 'right']:
+                            x = {'left':-1, 'right':1}[key]
+                            # get index number
+                            current_option_index = current_option_stack.index(current_option.title)
+    
+                            if current_option_index == 0:
+                                if x < 0:
+                                    self.tool_bar.highlighted_option.title = current_option_stack[- 1]
+                                    self.run_menu(input_ = False)
+                                if x > 0:
+                                    self.tool_bar.highlighted_option.title = current_option_stack[current_option_index + x]
+                                    self.run_menu(input_ = False)
+ 
+                            elif current_option_index  == len(current_option_stack) - 1:
+                                if x > 0:
+                                    self.tool_bar.highlighted_option.title = current_option_stack[0]
+                                    self.run_menu(input_ = False)
+                                if x < 0:
+                                    self.tool_bar.highlighted_option.title = current_option_stack[current_option_index + x]
+                                    self.run_menu(input_ = False)
+                            elif x + current_option_index in range(0, len(current_option_stack)):
+                                self.tool_bar.highlighted_option.title = current_option_stack[current_option_index + x]
+
+                                self.run_menu(input_ = False)
             if isinstance(self.highlighted_option, Menu): # checks if selection is menu
                 self.highlighted_option.run_menu()
             self.previous_execution = self.highlighted_option
@@ -540,14 +734,12 @@ class Menu():
         '''
             Refreshes the screen 
         '''
-
         self.run_menu()
 
     def back(self):
         '''
             Goes to the previous menu, if current menu does not have any previous menus then it refreshes the page
         '''
-
         if self.has_parent():
             self.parent_menu.refresh()
         else:
@@ -565,4 +757,3 @@ class Menu():
         '''
         self.clear_screen()
         sys.exit()
-
